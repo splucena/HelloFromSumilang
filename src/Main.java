79ec07ff9533482943b17b2377e44e99a0813f9c
splucena <sumilangplucena@yahoo.com.ph>
2020-10-08 18:31:35 +0800

    added main class

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Team 12: Eli, Evan, Ethan
+        System.out.println("Hello Eli, Evan, and Ethan, my name is Em.");
+    }
+}
