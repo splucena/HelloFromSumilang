003cd53993b48e8f7e1c98dcdac31b7a67f27dbe
ehandy <evan@handysoftware.com>
2020-10-08 13:22:43 -0400

    Added Evan's name

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         // Team 12: Eli, Evan, Ethan
         System.out.println("Hello Eli, Evan, and Ethan, my name is Em.");
+        System.out.println("Hello Em, my name is Evan.");
     }
 }
